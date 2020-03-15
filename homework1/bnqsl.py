d3e038a19f5cde8a38c7901b1a6532db8b72bcea
FFCXX <2082033549@qq.com>
2020-03-15 15:12:35 +0800

    删除了homework1

--- homework1/bnqsl.py
+++
@@ -1,9 +0,0 @@
-#6 前面2个元素为0，1，输出100以内的斐波那契数列；
-f1=0
-f2=1
-print(f1)
-print(f2)
-for i in range(1,101):
-    if i==f2+f1:
-        print(i)
-        f1,f2=f2,i
--- homework1/year.py
+++
@@ -1,8 +0,0 @@
-#4 判断用户输入的年份是否为闰年
-x=int(input("输入年份:"))
-if x%4==0 and x%100!=0:
-    print("是闰年")
-elif x%400==0:
-    print("是闰年")
-else:
-    print("不是闰年")
